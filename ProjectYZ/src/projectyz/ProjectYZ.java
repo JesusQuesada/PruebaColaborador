3d6a87e7c2e43913fd01d4d1d26baf02ffc5517e
jquesadaabeijon <jquesadaabeijon@atanasoff32.danielcastelao.org>
2016-10-10 09:53:46 +0200

    ProjectYZ

--- ProjectYZ/src/projectyz/ProjectYZ.java
+++ ProjectYZ/src/projectyz/ProjectYZ.java
@@ -15,7 +15,7 @@ public class ProjectYZ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 1ª modificación
     }
     
 }
