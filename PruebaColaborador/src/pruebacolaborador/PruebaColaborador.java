061174d072124dd606d0e5ce75aa846488800220
Jesus Quesada <jmqa82@gmail.com>
2016-10-13 09:49:27 +0200

    Update PruebaColaborador.java

--- PruebaColaborador/src/pruebacolaborador/PruebaColaborador.java
+++ PruebaColaborador/src/pruebacolaborador/PruebaColaborador.java
@@ -15,8 +15,8 @@ public class PruebaColaborador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //1ª modificación de la prueba
-        //2ª modificación de la prueba
+        //1ª modificación de la prueba nueva
+        //2ª modificación de la prueba nueva
 
     }
     
