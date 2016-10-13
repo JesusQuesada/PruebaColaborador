e90346ec21a0eaf217ee4b45e4bc9d09852f007f
jquesadaabeijon <jquesadaabeijon@atanasoff32.danielcastelao.org>
2016-10-13 09:51:18 +0200

    Modificación colaborador

--- PruebaColaborador/src/pruebacolaborador/PruebaColaborador.java
+++ PruebaColaborador/src/pruebacolaborador/PruebaColaborador.java
@@ -15,8 +15,8 @@ public class PruebaColaborador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //1ª modificación de la prueba
-        //2ª modificación de la prueba
+        //1ª modificación
+        //2ª modificación
 
     }
     
